11d787c3f0418ceee22ac32b33dbdce929b539dc
Ar2s <arti114@o2.pl>
2015-08-21 11:47:35 +0000

    Add report#subjects and Students private params

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,4 +2,9 @@ class ReportsController < ApplicationController
   before_action :authenticate_user!
   
   expose(:subject_items) { SubjectItem.includes(:teacher, :students) }
+  
+  def subjects
+  	@subject_items = SubjectItem.all
+  	render 'subjects'
+  end
 end
