9f5e214a1e70e2e79e556c895f92834efed0deda
Ar2s <arti114@o2.pl>
2015-08-21 11:21:57 +0000

    Added missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,15 @@ Rails.application.routes.draw do
     get :subjects
   end
   
+  resources :teachers do
+    get :subjects
+  end
+  
+  get 'reports/subjects' => 'reports#subjects', as: 'report_subjects'
+
+  resources :reports do
+    get :subjects
+  end
+  
   root 'visitors#index'
 end
